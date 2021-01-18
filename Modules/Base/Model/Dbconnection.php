a49a6810d7a6607cb4f4c0ae297cdc945287032e
kushtej <007kushtej@gmail.com>
2021-01-18 21:43:30 +0530

    Changed local database to remote database

--- Modules/Base/Model/Dbconnection.php
+++ Modules/Base/Model/Dbconnection.php
@@ -7,14 +7,14 @@ class Dbconnection {
     public static $conn = false;
 
     public static function connect(){
-      $serverName = "localhost";
-      $dbUserName = "root";
-      $dbPassword = "Kush007_tej";
-      $dbName = "atm";
-      // $serverName = "sql12.freemysqlhosting.net";
-      // $dbUserName = "sql12386553";
-      // $dbPassword = "bxEPaZwjRP";
-      // $dbName = "sql12386553";
+      // $serverName = "localhost";
+      // $dbUserName = "root";
+      // $dbPassword = "Kush007_tej";
+      // $dbName = "atm";
+      $serverName = "sql12.freemysqlhosting.net";
+      $dbUserName = "sql12386553";
+      $dbPassword = "bxEPaZwjRP";
+      $dbName = "sql12386553";
       $conn = mysqli_connect($serverName, $dbUserName, $dbPassword, $dbName);
       if ($conn->connect_error){
           return false;
